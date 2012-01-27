a992fac07f054cbdf9ee5fff885f1c0742a75204
Jonas Abreu <jonas@vidageek.net>
2012-01-27 15:21:21 -0200

    criando página de cadastro

---
+++ src/main/java/net/vidageek/games/regex/CadastroController.java
@@ -0,0 +1,14 @@
+package net.vidageek.games.regex;
+
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+final public class CadastroController {
+
+	@Get("/cadastro")
+	public void cadastro() {
+
+	}
+
+}
