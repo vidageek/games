d0de39a64c6e099c26936d3bb5ad9e945612356f
nj_all <narciso.benigno@gmail.com>
2012-01-31 21:30:35 -0200

    assert to verify if number of tasks and aswers are same

--- src/test/java/net/vidageek/games/regex/test/infra/Person.java
+++ src/test/java/net/vidageek/games/regex/test/infra/Person.java
@@ -12,6 +12,7 @@ final public class Person {
 	}
 
 	public void play(final Game game) {
+		assertTrue("Number of tasks and answers should be equals!", game.size() == this.answers.length);
 		for (int i = 0; i < game.size(); i++) {
 			assertTrue(	game.getClass().getSimpleName() + " task " + i + " is not correctly answered by " + answers[i],
 						game.task(i).judge(answers[i]).ok());
