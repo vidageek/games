6a01b2984e0a362907f6b9a5a24df4bd4ed7ed3d
nj_all <narciso.benigno@gmail.com>
2012-02-07 22:33:03 -0200

    refactoring test to use same fixture

--- src/test/java/net/vidageek/games/regex/task/NegateCharClassRegexTest.java
+++ src/test/java/net/vidageek/games/regex/task/NegateCharClassRegexTest.java
@@ -4,24 +4,30 @@ import static org.junit.Assert.assertEquals;
 import net.vidageek.games.task.JudgedTask;
 import net.vidageek.games.task.status.Failed;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class NegateCharClassRegexTest {
 
-	@Test
-	public void shouldReturnFailedWhenMatch() throws Exception {
+	private NegateCharClassRegex aNegateCharClassTask;
+
+	@Before
+	public void setup() throws Exception {
 		MatcherTargets cannotMatch = MatcherTargets.fromStrings("a", "b");
 		MatcherTargets shouldMatch = MatcherTargets.fromStrings("c", "d");
-		JudgedTask judge = new NegateCharClassRegex(cannotMatch, shouldMatch).judge("[^ef]");
+		aNegateCharClassTask = new NegateCharClassRegex(cannotMatch, shouldMatch);
+	}
+	
+	@Test
+	public void shouldReturnFailedWhenMatch() throws Exception {
+		JudgedTask judge = aNegateCharClassTask.judge("[^ef]");
 		assertEquals(Failed.class, judge.getClass());
 		assertEquals("Não deveria fazer match com \"a\"<br>Não deveria fazer match com \"b\"", judge.getReason());
 	}
 	
 	@Test
 	public void shouldShowWhatTargetMatchCannotMatchWhenFail() {
-		MatcherTargets negateClassWontMatch = MatcherTargets.fromStrings("a", "b");
-		MatcherTargets negateClassShouldMatch = MatcherTargets.fromStrings("c", "d");
-		JudgedTask judge = new NegateCharClassRegex(negateClassWontMatch, negateClassShouldMatch).judge("[^abcd]");
+		JudgedTask judge = aNegateCharClassTask.judge("[^abcd]");
 		assertEquals(Failed.class, judge.getClass());
 		assertEquals("\"[^abcd]\" não dá match em \"c\"<br>\"[^abcd]\" não dá match em \"d\"", judge.getReason());
 	}
