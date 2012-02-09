547470b3bd17564766e911ebe38cee174780fe9d
Jonas Abreu <jonas@vidageek.net>
2012-02-09 16:15:36 -0200

    adicionados passos do deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
+scp ~/artifacts/games/games.war ubuntu@177.71.178.115:~/.
+ssh ubuntu@177.71.178.115 "/bin/bash ~/jetty/bin/jetty.sh stop && cp ~/games.war ~/jetty/webapps/. && /bin/bash ~/jetty/bin/jetty.sh start"
 
