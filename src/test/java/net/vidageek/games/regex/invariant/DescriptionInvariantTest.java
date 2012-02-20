a02d6a78fd8bfeb8baf0dfe24d11c1ae4063af6f
Jonas Abreu <jonas@vidageek.net>
2012-02-19 22:43:35 -0200

    adicionado teste para impedir que RegEx seja escrito errado (estava me incomodando a falta de padrão. Eu sei, toc...)

--- src/test/java/net/vidageek/games/regex/invariant/DescriptionInvariantTest.java
+++ src/test/java/net/vidageek/games/regex/invariant/DescriptionInvariantTest.java
@@ -1,6 +1,10 @@
 package net.vidageek.games.regex.invariant;
 
 import static org.junit.Assert.assertTrue;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.vidageek.games.regex.invariant.infra.DescriptionInvariant;
 import net.vidageek.games.regex.invariant.infra.Invariant;
 
@@ -16,6 +20,18 @@ final public class DescriptionInvariantTest {
 			public void apply(final String content) throws Throwable {
 				assertTrue(	" Found invalid chars: [" + content.replaceAll(ALLOWED_CHARS, "") + "]",
 							containsInvalidChars(content));
+			}
+		});
+	}
+
+	@Test
+	public void regexMustBeProperWritten() throws Throwable {
+		new DescriptionInvariant().run(new Invariant() {
+			public void apply(final String content) throws Throwable {
+				Matcher matcher = Pattern.compile("(?i)(regex)").matcher(content);
+				while (matcher.find()) {
+					assertTrue("Found invalid way of writing RegEx", "RegEx".equals(matcher.group(1)));
+				}
 			}
 		});
 	}
