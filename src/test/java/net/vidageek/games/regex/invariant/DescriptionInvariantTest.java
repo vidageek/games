4fc3ce0adb86dc72316687d98c04e0b609026e99
Jonas Abreu <jonas@vidageek.net>
2012-03-05 23:07:06 -0300

    usando invariant runner para testes de invariant

--- src/test/java/net/vidageek/games/regex/invariant/DescriptionInvariantTest.java
+++ src/test/java/net/vidageek/games/regex/invariant/DescriptionInvariantTest.java
@@ -5,35 +5,30 @@ import static org.junit.Assert.assertTrue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import net.vidageek.games.regex.invariant.infra.DescriptionInvariant;
-import net.vidageek.games.regex.invariant.infra.Invariant;
+import net.vidageek.invariant.FileData;
+import net.vidageek.invariant.Invariant;
+import net.vidageek.invariant.InvariantRunner;
 
-import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(InvariantRunner.class)
 final public class DescriptionInvariantTest {
 
 	private static final String ALLOWED_CHARS = "[\\w\\s\\Q<>&/.,();+=^:\\?[]-${}|*\\E]*";
 
-	@Test
-	public void allDescriptionsMustUseHtmlEntities() throws Throwable {
-		new DescriptionInvariant().run(new Invariant() {
-			public void apply(final String content) throws Throwable {
-				assertTrue(	" Found invalid chars: [" + content.replaceAll(ALLOWED_CHARS, "") + "]",
-							containsInvalidChars(content));
-			}
-		});
+	@Invariant(affects = ".*\\.html", folder = "src/main/resources")
+	public void allDescriptionsMustUseHtmlEntities(final FileData data) throws Throwable {
+		final String content = data.getContent();
+		assertTrue(	" Found invalid chars: [" + content.replaceAll(ALLOWED_CHARS, "") + "]",
+					containsInvalidChars(content));
 	}
 
-	@Test
-	public void regexMustBeProperWritten() throws Throwable {
-		new DescriptionInvariant().run(new Invariant() {
-			public void apply(final String content) throws Throwable {
-				Matcher matcher = Pattern.compile("(?i)(regex)").matcher(content);
-				while (matcher.find()) {
-					assertTrue("Found invalid way of writing RegEx", "RegEx".equals(matcher.group(1)));
-				}
-			}
-		});
+	@Invariant(affects = ".*\\.html", folder = "src/main/resources")
+	public void regexMustBeProperWritten(final FileData data) throws Throwable {
+		Matcher matcher = Pattern.compile("(?i)(regex)").matcher(data.getContent());
+		while (matcher.find()) {
+			assertTrue("Found invalid way of writing RegEx", "RegEx".equals(matcher.group(1)));
+		}
 	}
 
 	private boolean containsInvalidChars(final String content) throws Throwable {
--- src/test/java/net/vidageek/games/regex/invariant/infra/Invariant.java
+++
@@ -1,7 +0,0 @@
-package net.vidageek.games.regex.invariant.infra;
-
-public interface Invariant {
-
-	void apply(String content) throws Throwable;
-
-}
