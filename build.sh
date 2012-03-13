27880d935b74eb0e9208541ca2b75778c28b89da
Jonas Abreu <jonas@vidageek.net>
2012-03-13 16:18:33 -0300

    modificando script de build para usar gradle do path

--- build.sh
+++ build.sh
@@ -1,12 +1,4 @@
 #! /bin/bash
 
-export GRADLE_OPTS="-XX:ThreadStackSize=256k -XX:+UseCompressedOops" && /home/ubuntu/gradle/bin/gradle -i test assemble
-
-BUILD=$?
-
-if [ $BUILD -ne 0 ]; then
-	exit $BUILD;
-fi;
-
-cp build/libs/games-0.1-SNAPSHOT.war ~/artifacts/games/games.war
+gradle -i test assemble
 
