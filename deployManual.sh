9d61cabbdeb79e95c35cc404a6c6bf7dc71208c8
Jonas Abreu <jonas@vidageek.net>
2012-03-16 15:15:16 -0300

    testando commit hook do cloudbees

--- deployManual.sh
+++ deployManual.sh
@@ -8,4 +8,3 @@ scp -i ~/.ssh/games.pem build/libs/games* ubuntu@177.71.178.115:~/games.war
 
 echo "Fazendo deploy"
 ssh -i ~/.ssh/games.pem ubuntu@177.71.178.115 "/bin/bash ~/jetty/bin/jetty.sh stop && cp ~/games.war ~/jetty/webapps/. && /bin/bash ~/jetty/bin/jetty.sh start"
-
