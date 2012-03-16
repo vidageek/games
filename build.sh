e5373cb1dbb1edcf9f1d43518eb11c1030daa171
Jonas Abreu <jonas@vidageek.net>
2012-03-16 14:41:58 -0300

    tentativa de fazer o build rodar no cloudbees

--- build.sh
+++ build.sh
@@ -2,3 +2,11 @@
 
 gradle -i test assemble
 
+BUILD=$?
+
+if [ $BUILD -ne 0 ]; then
+       exit $BUILD;
+fi;
+
+scp -i /private/games.pem build/libs/games-0.1-SNAPSHOT.war ubuntu@games.vidagek.net:~/games.war
+
