1ff3e14f315559d69dd960b6cdfe890e81cbb33a
Jonas Abreu <jonas@vidageek.net>
2012-03-16 15:06:12 -0300

    atualizando deploy.sh para rodar no cloudbees

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
-scp ~/artifacts/games/games.war ubuntu@177.71.178.115:~/.
-ssh ubuntu@177.71.178.115 "/bin/bash ~/jetty/bin/jetty.sh stop && cp ~/games.war ~/jetty/webapps/. && /bin/bash ~/jetty/bin/jetty.sh start"
+ssh -i /private/vidageek/games.pem ubuntu@177.71.178.115 "/bin/bash ~/jetty/bin/jetty.sh stop && cp ~/games.war ~/jetty/webapps/. && /bin/bash ~/jetty/bin/jetty.sh start"
 
