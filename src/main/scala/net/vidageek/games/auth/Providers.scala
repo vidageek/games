eebcb54dce112feb0064254b9d95b3ac75d29856
Jonas Abreu <jonas@vidageek.net>
2012-03-18 22:44:23 -0300

    removendo implementação de Iterable

--- src/main/scala/net/vidageek/games/auth/Providers.scala
+++ src/main/scala/net/vidageek/games/auth/Providers.scala
@@ -8,17 +8,15 @@ import scala.collection.JavaConversions._
 
 import com.google.common.collect.Maps
 
-class Providers(authProviders: Collection[AuthProvider]) extends Iterable[AuthProvider] {
-  val providers: Map[String, AuthProvider] = Maps.newConcurrentMap()
-  authProviders.foreach(add(_))
+class Providers(authProviders : Collection[AuthProvider]) {
+    val providers : Map[String, AuthProvider] = Maps.newConcurrentMap()
+    authProviders.foreach(add(_))
 
-  def add(provider: AuthProvider) {
-    providers.put(provider.name, provider)
-  }
+    def add(provider : AuthProvider) {
+        providers.put(provider.name, provider)
+    }
 
-  def quantity = providers.size()
+    def quantity = providers.size()
 
-  def iterator: Iterator[AuthProvider] = Collections.unmodifiableCollection(providers.values).iterator
-
-  def byName(name: String) = providers.get(name)
+    def byName(name : String) = providers.get(name)
 }
