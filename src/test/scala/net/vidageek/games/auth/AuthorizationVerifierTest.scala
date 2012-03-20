1d226a83239a2f15e5b74449deb115353732ec21
Paulo Siqueira <paulo.siqueira@gmail.com>
2012-03-19 23:13:05 -0300

    removed dependency on junit

--- src/test/scala/net/vidageek/games/auth/AuthorizationVerifierTest.scala
+++ src/test/scala/net/vidageek/games/auth/AuthorizationVerifierTest.scala
@@ -1,12 +1,8 @@
 package net.vidageek.games.auth
 
-import org.junit.Test
 import org.specs2.mutable._
-import org.junit.runner.RunWith
-import org.specs2.runner.JUnitRunner
 
-@RunWith(classOf[JUnitRunner])
-class AuthorizationVerifierTest extends SpecificationWithJUnit{
+class AuthorizationVerifierTest extends Specification {
   
   "Authorization Verifier" should {
     "authorize with valid verifier" in {
