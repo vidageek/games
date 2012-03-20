f51b3a58b1bc15b64117e4f4c9c544adebf74901
nj_all <narciso.benigno@gmail.com>
2012-03-20 00:27:58 -0300

    adição de ignore para arquivos do intellij

---
+++ project/Build.scala
@@ -0,0 +1,20 @@
+import sbt._
+import Keys._
+
+object TasksBuild extends Build {
+  val css = TaskKey[Unit]("css", "Resolve GZip to CSS")
+
+val gzipCss = css := {
+  //FileUtilities.copyFile( webAppDir + "css/**/*.css",
+   // webAppDir + "css/games-packaged.css.gz",
+    //                         log)
+    //log.info("Copied jetty-web.xml into place")
+    None
+} 
+
+lazy val project = Project (
+    "project",
+    file ("."),
+    settings = Defaults.defaultSettings ++ Seq(gzipCss)
+  )
+}
