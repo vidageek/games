95b66a696d21b7e18197165c1d7ffe18ddf63975
Juliano Alves <von.juliano@gmail.com>
2012-03-19 23:57:13 -0300

    mudado para Map imutavel

--- src/main/scala/net/vidageek/games/auth/Providers.scala
+++ src/main/scala/net/vidageek/games/auth/Providers.scala
@@ -3,13 +3,13 @@ package net.vidageek.games.auth
 import java.util.Collection
 
 import scala.collection.JavaConversions.collectionAsScalaIterable
-import scala.collection.mutable.Map
+import scala.collection.immutable.Map
 
 class Providers(authProviders: Collection[AuthProvider]) {
-  val providers: Map[String, AuthProvider] = Map()
+  var providers: Map[String, AuthProvider] = Map()
   authProviders.foreach(add(_))
 
-  def add(provider: AuthProvider): Unit = providers += ((provider.name, provider))
+  def add(provider: AuthProvider): Unit = providers = providers + ((provider.name, provider))
 
   def quantity = providers.size
 
