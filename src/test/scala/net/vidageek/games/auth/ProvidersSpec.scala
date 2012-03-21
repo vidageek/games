505a24f6cb3bac232daed731380e7152b3e3213b
Paulo Siqueira <paulo.siqueira@gmail.com>
2012-03-21 03:37:40 -0300

    migrated Test to Spec

---
+++ src/test/scala/net/vidageek/games/auth/ProvidersSpec.scala
@@ -0,0 +1,27 @@
+package net.vidageek.games.auth
+
+import net.vidageek.games.auth.twitter.TwitterAuthProvider
+import org.specs2.mock.Mockito
+import org.specs2.mutable.Specification
+
+class ProvidersSpec extends Specification with Mockito {
+  val twitterAuthPovider = mock[TwitterAuthProvider]
+  val aTestAuthProvider = mock[AuthProvider]
+
+  twitterAuthPovider.name returns "twitter"
+  aTestAuthProvider.name returns "TestProvider"
+  
+  val providers = new Providers(List(twitterAuthPovider, aTestAuthProvider))
+  
+  "providers" should {
+    "have the twitter provider" in {
+      providers.quantity must_== 2
+      providers.byName("twitter") must_== twitterAuthPovider
+      providers.byName("TestProvider") must_== aTestAuthProvider
+    }
+    
+    "find the twitter provider" in {
+      providers.byName(twitterAuthPovider.name) must_== twitterAuthPovider
+    }
+  }
+}
