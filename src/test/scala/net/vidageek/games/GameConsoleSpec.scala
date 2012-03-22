767dc1eafefc031c8d57a6ffb672e3998ba70b32
Jonas Abreu <jonas@vidageek.net>
2012-03-21 23:49:36 -0300

    adicinada anotação para que spec rode como um teste junit, voltando a ficar integrado no build contínuo.

--- src/test/scala/net/vidageek/games/GameConsoleSpec.scala
+++ src/test/scala/net/vidageek/games/GameConsoleSpec.scala
@@ -1,13 +1,16 @@
 package net.vidageek.games
 
 import br.com.caelum.vraptor.Result
-import net.vidageek.games.task.status.{Failed, Ok}
+import net.vidageek.games.task.status.{ Failed, Ok }
 import org.mockito.Mockito.withSettings
 import org.mockito.Answers.RETURNS_DEEP_STUBS
 import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 import org.specs2.specification.Scope
+import org.specs2.runner.JUnitRunner
+import org.junit.runner.RunWith
 
+@RunWith(classOf[JUnitRunner])
 class GameConsoleSpec extends Specification with Mockito {
   "a game console" should {
     "redirect to same task when task is incorrect" in new GameConsoleScope {
@@ -20,7 +23,7 @@ class GameConsoleSpec extends Specification with Mockito {
       there was one(result).include("judgedTask", judgedTask)
       there was one(result).include("challenge", "challenge")
     }
-    
+
     "redirect to game begin if there are no more tasks" in new GameConsoleScope {
       val judgedTask = new Ok()
       game.task(3).judge("challenge") returns judgedTask
@@ -31,7 +34,7 @@ class GameConsoleSpec extends Specification with Mockito {
       there was one(gameConsole).index("name")
       there was one(result).include("judgedTask", judgedTask)
     }
-    
+
     "redirect to next task if task is ok" in new GameConsoleScope {
       val judgedTask = new Ok()
       game.task(3).judge("challenge") returns judgedTask
@@ -44,12 +47,12 @@ class GameConsoleSpec extends Specification with Mockito {
       there was one(result).include("judgedTask", judgedTask)
     }
   }
-  
+
   trait GameConsoleScope extends Scope {
     val result = mock[Result]
     val game = mock[Game](withSettings.defaultAnswer(RETURNS_DEEP_STUBS.get))
     val gameConsole = mock[GameConsole]
-  
+
     result.redirectTo(any[GameConsole]) returns gameConsole
   }
 }
--- src/test/scala/net/vidageek/games/auth/AuthorizationVerifierSpec.scala
+++ src/test/scala/net/vidageek/games/auth/AuthorizationVerifierSpec.scala
@@ -1,14 +1,17 @@
 package net.vidageek.games.auth
 
 import org.specs2.mutable._
+import org.specs2.runner.JUnitRunner
+import org.junit.runner.RunWith
 
+@RunWith(classOf[JUnitRunner])
 class AuthorizationVerifierSpec extends Specification {
-  
+
   "Authorization Verifier" should {
     "authorize with valid verifier" in {
       AuthorizationVerifier("validVerifier").authorized must beTrue
     }
-    
+
     "not authorize" in {
       AuthorizationVerifier(null).authorized must beFalse
     }
