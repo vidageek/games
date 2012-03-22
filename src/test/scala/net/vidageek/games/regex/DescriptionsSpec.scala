026038135624f109d88464ee55692a562d67f4c2
Jonas Abreu <jonas@vidageek.net>
2012-03-21 23:51:38 -0300

    adicionando anotação para que testes rodem no build contínuo

--- src/test/scala/net/vidageek/games/regex/DescriptionsSpec.scala
+++ src/test/scala/net/vidageek/games/regex/DescriptionsSpec.scala
@@ -1,18 +1,21 @@
 package net.vidageek.games.regex
 
 import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+import org.junit.runner.RunWith
 
+@RunWith(classOf[JUnitRunner])
 class DescriptionsSpec extends Specification {
 
   "descriptions" should {
     "read description according to group name" in {
       "test <br />" must_== new Descriptions().forGroup("test")
     }
-    
+
     "return no description for group when group has no description" in {
       "No description for group test.not.exists" must_== new Descriptions().forGroup("test.not.exists")
     }
-    
+
     "cache description to avoid lookup" in {
       val descriptions = new Descriptions()
       descriptions.forGroup("test") must be(descriptions.forGroup("test"))
--- src/test/scala/net/vidageek/games/regex/RegexSpec.scala
+++ src/test/scala/net/vidageek/games/regex/RegexSpec.scala
@@ -2,15 +2,18 @@ package net.vidageek.games.regex;
 
 import net.vidageek.games.regex.task.Regex
 import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+import org.junit.runner.RunWith
 
+@RunWith(classOf[JUnitRunner])
 class RegexTest extends Specification {
-  
+
   "regex" should {
     "find a group" in {
       val matchAtGroup = "aaaa"
       new Regex("b+([a]+)c+").group(1).from("bbb" + matchAtGroup + "c") must_== matchAtGroup
     }
-    
+
     "throw an IlegalArgumentException when group doesn't match" in {
       new Regex("b+([a]+)c+").group(1).from("bbbc") must throwAn[IllegalArgumentException](
         message = "could not match text.")
