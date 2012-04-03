891df793b9e221f40c265a4042fcd97004b9973b
Juliano Alves <von.juliano@gmail.com>
2012-04-02 21:14:36 -0300

    login com google account

---
+++ src/main/scala/net/vidageek/games/auth/GoogleAuthProvider.scala
@@ -0,0 +1,23 @@
+package net.vidageek.games.auth
+
+import net.vidageek.games.vraptor.OAuthSecrets
+import org.scribe.model.Verifier
+import org.scribe.model.Token
+
+class GoogleAuthProvider(secrets: OAuthSecrets) extends AuthProvider {
+  val googleService = OAuthServiceBuilder(this, secrets)
+  var accessToken: Token = _
+
+  override def applicationAuthorizationUrl = googleService.autorizationUrl
+
+  override def name = "google"
+
+  override def accessToken(verifier: Verifier) = {
+    accessToken = googleService.accessToken(verifier)
+    accessToken
+  }
+
+  override def userName: String = ""
+
+  override def logout: Unit = {}
+}
