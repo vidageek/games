e9cf28adf911ff9589fa53928f4f64e265af50bb
nj_all <narciso.benigno@gmail.com>
2012-04-17 01:02:30 -0300

    movendo capture group para scala

---
+++ src/main/scala/net/vidageek/games/regex/task/CaptureGroup.scala
@@ -0,0 +1,59 @@
+package net.vidageek.games.regex.task;
+
+import scala.collection.mutable._
+
+import net.vidageek.games.regex.task.MatcherTargets._;
+
+import java.util.List
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+import net.vidageek.games.task.JudgedTask
+import net.vidageek.games.task.Task
+import net.vidageek.games.task.status.Ok
+
+import com.google.common.collect.Lists
+
+
+class CaptureGroup(val matchingTarget: String, val captureGroupTargets: String*)  extends Task {
+
+	val validations = ListBuffer[GroupValidation]()
+	addAllValidations()
+	
+	def addAllValidations(): Unit = {
+		validations += new ValidationIfMatch(matchingTarget)
+		validations += new ValidationIfAllCapture(captureGroupTargets.toList : _*)
+		validations += new ValidationCaptureCorrectGroup(matchingTarget)
+		validations += new ValidationIfAllGroupsMatch(captureGroupTargets.toList : _*)
+	}
+
+	def judge(challenge: String): JudgedTask = {
+		val matcher: Matcher = Pattern.compile(challenge).matcher(matchingTarget);
+		return applyAllValidations(challenge, matcher);
+
+	}
+
+	def applyAllValidations(challenge: String, matcher: Matcher): JudgedTask = {
+		var judge: JudgedTask = new Ok()
+		for (validation  <- validations) {
+			judge = validation.judge(challenge, matcher);
+			if (!judge.getOk()) {
+				return judge;
+			}
+		}
+		judge;
+	}
+
+	def getChallenge: String = {
+		"Qual regex d&aacute; match em " + from(matchingTarget).asHtml() + " e captura " + captureTarget + "?"
+	}
+
+	def captureTarget: String = {
+		from(captureGroupTargets.toList : _*).asHtml()
+	}
+
+	override def toString(): String = {
+		getChallenge
+	}
+
+}
--- src/main/scala/net/vidageek/games/regex/task/ValidationIfAllCapture.scala
+++ src/main/scala/net/vidageek/games/regex/task/ValidationIfAllCapture.scala
@@ -1,6 +1,6 @@
 package net.vidageek.games.regex.task;
 
-import static net.vidageek.games.regex.task.MatcherTargets.from;
+import net.vidageek.games.regex.task.MatcherTargets._;
 
 import java.util.regex.Matcher;
 
@@ -8,15 +8,9 @@ import net.vidageek.games.task.JudgedTask;
 import net.vidageek.games.task.status.Failed;
 import net.vidageek.games.task.status.Ok;
 
-public class ValidationIfAllCapture implements GroupValidation {
+class ValidationIfAllCapture(captureGroupTargets: String*) extends GroupValidation {
 
-	private final String[] captureGroupTargets;
-
-	public ValidationIfAllCapture(final String... captureGroupTargets) {
-		this.captureGroupTargets = captureGroupTargets;
-	}
-
-	public JudgedTask judge(final String challenge, final Matcher matcher) {
+	def judge(challenge: String, matcher: Matcher): JudgedTask = {
 		if (matcher.groupCount() != captureGroupTargets.length) {
 			return new Failed("A regex " + from(challenge).asHtml() + " n&atilde;o cont&eacute;m "
 					+ captureGroupTargets.length + " grupo(s) de captura.");
