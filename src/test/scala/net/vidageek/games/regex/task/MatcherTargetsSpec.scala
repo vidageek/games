260fb81cd587d449e963da9af333c17dad036f19
Paulo Siqueira <paulo.siqueira@gmail.com>
2012-05-29 23:57:24 -0300

    removed unused import

--- src/test/scala/net/vidageek/games/regex/task/MatcherTargetsSpec.scala
+++ src/test/scala/net/vidageek/games/regex/task/MatcherTargetsSpec.scala
@@ -4,7 +4,6 @@ import net.vidageek.games.regex.task.MatcherTargets.from
 import org.junit.runner.RunWith
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
-import scala.collection.JavaConverters._
 
 @RunWith(classOf[JUnitRunner])
 class MatcherTargetsSpec extends Specification {
