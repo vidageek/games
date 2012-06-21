9c04fcd96ea002b4d2448d94eb28172e65fde014
Jonas Abreu <jonas@vidageek.net>
2012-06-21 16:58:06 -0300

    fazendo teste compilar

--- src/test/scala/net/vidageek/games/auth/AuthenticatedRequesterSpec.scala
+++ src/test/scala/net/vidageek/games/auth/AuthenticatedRequesterSpec.scala
@@ -1,20 +1,22 @@
 package net.vidageek.games.auth
 
-import org.specs2.mutable.Specification
-import org.specs2.mock.Mockito
+import org.scribe.model.{Token, Response, OAuthRequest}
 import org.scribe.oauth.OAuthService
-import org.scribe.model.{OAuthRequest, Response, Token}
+import org.specs2.mock.Mockito
+import org.specs2.mutable.Specification
 import org.specs2.specification.Scope
+
+import vggames.auth.AuthenticatedRequester
 
 class AuthenticatedRequesterSpec extends Specification {
   "Authentication Requester" should {
     "sends a get" in new RequesterInformations {
-      val response:Response = requester.get("http://www.google.com.br")
+      val response : Response = requester.get("http://www.google.com.br")
       (response.getCode / 100) should_== 2
     }
 
     "sends a post" in new RequesterInformations {
-      val response:Response = requester.post("http://www.google.com.br")
+      val response : Response = requester.post("http://www.google.com.br")
       (response.getCode / 100) should_== 2
     }
 
@@ -26,7 +28,7 @@ class AuthenticatedRequesterSpec extends Specification {
 }
 
 trait RequesterInformations extends Scope with Mockito {
-  var accessToken:Token = mock[Token]
-  var oauthService:OAuthService = mock[OAuthService]
-  var requester:AuthenticatedRequester = AuthenticatedRequester(accessToken, oauthService)
+  var accessToken : Token = mock[Token]
+  var oauthService : OAuthService = mock[OAuthService]
+  var requester : AuthenticatedRequester = AuthenticatedRequester(accessToken, oauthService)
 }
