1bc32186356eeda237490ed23900ce07c3a2f241
Jonas Abreu <jonas@vidageek.net>
2012-06-28 16:20:55 -0300

    removendo warnign

--- src/main/scala/vggames/scala/CodeProcessor.scala
+++ src/main/scala/vggames/scala/CodeProcessor.scala
@@ -3,7 +3,7 @@ package vggames.scala
 import com.twitter.util.Eval
 
 class CodeProcessor {
-  def processCode(code: String): Boolean = {
-    Eval[Boolean](code)
+  def processCode(code : String) : Boolean = {
+    new Eval(None).apply[Boolean](code)
   }
 }
