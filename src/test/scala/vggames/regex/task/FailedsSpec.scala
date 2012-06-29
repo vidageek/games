ee8992ff881a299277cbaeabb6994e4744f8ad02
Jonas Abreu <jonas@vidageek.net>
2012-06-29 18:58:20 -0300

    removendo Joiner

--- src/test/scala/vggames/regex/task/FailedsSpec.scala
+++ src/test/scala/vggames/regex/task/FailedsSpec.scala
@@ -1,13 +1,10 @@
 package vggames.regex.task
 
-
-import com.google.common.base.Joiner
 import org.junit.runner.RunWith
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
-import scala.collection.JavaConverters._
-import vggames.shared.task.status.Faileds
-import vggames.shared.task.status.Failed
+
+import vggames.shared.task.status.{ Faileds, Failed }
 
 @RunWith(classOf[JUnitRunner])
 class FailedsSpec extends Specification {
@@ -15,15 +12,15 @@ class FailedsSpec extends Specification {
     val faileds = new Faileds()
     faileds.addOnlyJudgedFailed(new Failed("[aPattern] não dá match em [matchingTarget]"))
     faileds.addOnlyJudgedFailed(new Failed("[aPattern1] não dá match em [matchingTarget1]"))
-    
+
     "show description to any faileds" in {
-      Joiner.on("<br />").join(faileds asJava) must_== faileds.getReason()
+      faileds.mkString("<br />") must_== faileds.getReason()
     }
 
     "show description to all faileds" in {
       val testFaileds = new Faileds()
       testFaileds.addAll(faileds)
-      Joiner.on("<br />").join(testFaileds asJava) must_== testFaileds.getReason()
+      testFaileds.mkString("<br />") must_== testFaileds.getReason()
     }
   }
 }
