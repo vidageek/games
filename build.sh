8765383cc2a37897ffcbfa75e65f7edaef6639ef
Jonas Abreu <jonas@vidageek.net>
2012-06-29 18:48:26 -0300

    tentando descobrir qual flag crasha o cloudbees

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 SBT_JAR=/opt/local/share/sbt/sbt-launch.jar
 SBT_JAR=/opt/sbt/sbt-launch-0.11.3-2.jar
 
-SBT="java -Xmx512m -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=372m -XX:+UseCompressedOops -jar $SBT_JAR"
+SBT="java -XX:MaxPermSize=372m -XX:+UseCompressedOops -jar $SBT_JAR"
 
 BUILD=0
 
