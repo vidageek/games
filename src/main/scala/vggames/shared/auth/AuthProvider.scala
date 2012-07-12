f67a55b0ca7b75ddc16b0e17aa8a0ce66a9200f9
njalldev <narciso.benigno@gmail.com>
2012-07-12 00:04:00 -0300

    melhorando os requestes para esconder os tokens, isolando o que vai ser gravado no prevayler

--- src/main/scala/vggames/shared/auth/AuthProvider.scala
+++ src/main/scala/vggames/shared/auth/AuthProvider.scala
@@ -1,11 +1,11 @@
 package vggames.shared.auth
 
-import org.scribe.model.{Verifier, Token}
+import org.scribe.model.Verifier
 
 trait AuthProvider {
   def applicationAuthorizationUrl: String
   def name: String
-  def accessToken(verifier: Verifier): Token
+  def accessToken(verifier: Verifier)
   def userName: String
   def logout: Unit
 }
--- src/main/scala/vggames/shared/auth/twitter/TwitterAuthProvider.scala
+++ src/main/scala/vggames/shared/auth/twitter/TwitterAuthProvider.scala
@@ -11,26 +11,23 @@ class TwitterAuthProvider(secrets : OAuthSecrets) extends AuthProvider {
 
   val twitterService = OAuthServiceBuilder(this, secrets)
   var accessToken : Token = _
-  var requester : AuthenticatedRequester = null
+  var requester : Option[AuthenticatedRequester] = None
 
   def applicationAuthorizationUrl = twitterService.autorizationUrl
 
   override def name = "twitter"
 
   override def accessToken(verifier : Verifier) = {
-    accessToken = twitterService.accessToken(verifier)
-    requester = new AuthenticatedRequester(accessToken, twitterService.authService)
-    accessToken
+    requester = Some(new AuthenticatedRequester(twitterService.accessToken(verifier), twitterService.authService))
   }
 
   override def logout {
-    requester.post("https://api.twitter.com/1/account/end_session.json")
+    requester.get.post("https://api.twitter.com/1/account/end_session.json")
   }
 
   override def userName : String = {
-    extractUserName(requester.get("http://api.twitter.com/1/account/verify_credentials.json"))
+    extractUserName(requester.get.get("http://api.twitter.com/1/account/verify_credentials.json"))
   }
-  private def sign(request : OAuthRequest) = twitterService.authService.signRequest(accessToken, request)
 
   private def extractUserName(response : Response) : String = {
     JSON.parseFull(response.getBody) match {
