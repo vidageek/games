dcecb24349636d2f9fa47bc2c793e4ab15dc8430
Jonas Abreu <jonas@vidageek.net>
2012-07-20 16:47:33 -0300

    adicionado css game

---
+++ src/main/scala/vggames/css/CssGame.scala
@@ -0,0 +1,22 @@
+package vggames.css
+
+import vggames.shared.task.Descriptions
+import br.com.caelum.vraptor.ioc.Component
+import vggames.shared.Game
+import vggames.regex.task.Tasks
+
+@Component
+class CssGame(descriptions : Descriptions) extends Game {
+
+  val tasks : Tasks = new Tasks
+
+  addTestTask
+
+  def addTestTask = {
+  }
+
+  def getDescription = "Um jogo muito legal para aprender CSS"
+
+  def getName : String = "CSS"
+
+}
--- src/main/scala/vggames/shared/vraptor/GameFactory.scala
+++ src/main/scala/vggames/shared/vraptor/GameFactory.scala
@@ -6,6 +6,7 @@ import vggames.regex.task.RegexGame
 import vggames.shared.Game
 import vggames.shared.task.Descriptions
 import vggames.scala.ScalaGame
+import vggames.css.CssGame
 
 @Component
 class GameFactory(descriptions : Descriptions, data : RequestData) extends ComponentFactory[Game] {
@@ -14,6 +15,7 @@ class GameFactory(descriptions : Descriptions, data : RequestData) extends Compo
     data.game match {
       case "regex" => new RegexGame(descriptions)
       case "scala" => new ScalaGame(descriptions)
+      case "css" => new CssGame(descriptions)
       case other => throw new RuntimeException("Não foi possível criar o jogo [" + other + "]. Talvez " +
         "seja necessário registrá-lo na GameFactory")
     }
