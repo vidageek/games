92692c9e56e13b2dc9f28085c43e8cc5a9291eaa
Jonas Abreu <jonas@vidageek.net>
2012-07-27 10:19:28 -0300

    começando css game

--- src/main/scala/vggames/css/CssGame.scala
+++ src/main/scala/vggames/css/CssGame.scala
@@ -4,6 +4,7 @@ import vggames.shared.task.Descriptions
 import br.com.caelum.vraptor.ioc.Component
 import vggames.shared.Game
 import vggames.shared.task.Tasks
+import vggames.shared.task.TaskGroup
 
 @Component
 class CssGame(descriptions : Descriptions) extends Game {
@@ -13,6 +14,9 @@ class CssGame(descriptions : Descriptions) extends Game {
   addTestTask
 
   def addTestTask = {
+    val group = new TaskGroup("Css test", "css.test", descriptions)
+    group.add(new CssTask("bla"))
+    tasks.add(group)
   }
 
   def getDescription = "Um jogo muito legal para aprender CSS"
---
+++ src/main/scala/vggames/css/CssTask.scala
@@ -0,0 +1,13 @@
+package vggames.css
+
+import vggames.shared.task.Task
+import vggames.shared.task.JudgedTask
+import vggames.shared.task.status.Ok
+
+class CssTask(challenge : String) extends Task {
+
+  def judge(challenge : String) : JudgedTask = Ok()
+
+  def getChallenge = challenge
+
+}
