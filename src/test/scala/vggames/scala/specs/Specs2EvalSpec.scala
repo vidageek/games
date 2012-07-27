dc1ad3cfb549af516c57e1c8923e3a6518362755
Paulo Siqueira <paulo.siqueira@gmail.com>
2012-07-27 19:49:03 -0300

    changed identantion a bit changed some specs descriptions to better suit what is really expected

--- src/test/scala/vggames/scala/specs/Specs2EvalSpec.scala
+++ src/test/scala/vggames/scala/specs/Specs2EvalSpec.scala
@@ -29,60 +29,50 @@ class Specs2EvalSpec extends Specification {
   }
 
   "Simple Eval" should {
-    "throw exception for unsafe code" in {
+    "fail for unsafe code" in {
       val fail = Specs2Eval(new TestSpec("exit")).judge("System.exit(-1);1")
       fail.getReason must contain("Tentativa de executar c&oacute;digo privilegiado dentro de uma task.")
     }
 
-    "throw exception for file access" in {
+    "fail for file access" in {
       val fail = Specs2Eval(new TestSpec("file")).judge("""new java.io.File(".").exists;1""")
       fail.getReason must contain("Tentativa de executar c&oacute;digo privilegiado dentro de uma task.")
     }
 
-    "throw exception for web request attempt" in {
+    "fail for web request attempt" in {
       val fail = Specs2Eval(new TestSpec("conn")).judge("""new java.net.URL("http://www.google.com.br/").openConnection;1""")
       fail.getReason must contain("Tentativa de executar c&oacute;digo privilegiado dentro de uma task.")
     }
 
-    "throw exception for classloader creation" in {
+    "fail for classloader creation" in {
       val fail = Specs2Eval(new TestSpec("classloader")).judge("""new java.net.URLClassLoader(Array[java.net.URL]());1""")
       fail.getReason must contain("Tentativa de executar c&oacute;digo privilegiado dentro de uma task.")
     }
 
-    "throw exception for security manager modification attempt" in {
+    "fail for security manager modification attempt" in {
       val fail = Specs2Eval(new TestSpec("sec manager")).judge("""System.setSecurityManager(null);1""")
       fail.getReason must contain("Tentativa de executar c&oacute;digo privilegiado dentro de uma task.")
     }
 
-    "throw exception for exception catching attempt" in {
+    "fail for exception catching attempt" in {
       val fail = Specs2Eval(new TestSpec("catch")).judge("""try {} catch {case _ => };1""")
       fail.getReason must contain("Tentativa de executar c&oacute;digo privilegiado dentro de uma task.")
     }
 
-    "throw exception for finally (can be used to stop exception propagation)" in {
+    "fail for finally (can be used to stop exception propagation)" in {
       val fail = Specs2Eval(new TestSpec("finally")).judge("""try {} finally {return ""};1""")
       fail.getReason must contain("Tentativa de executar c&oacute;digo privilegiado dentro de uma task.")
     }
   }
 
   class TestSpec(c : String) extends GameSpecification[RestrictedFunction2[Int, Int, Int]] {
-
     def runSignature = "(a:Int, b:Int):Int"
-
     def extendsType = "RestrictedFunction2[Int, Int, Int]"
 
     def challenge = c
-
     "a" should {
-      "b" in {
-        code(1, 2) must_== 3
-      }
-
-      "c" in {
-        code(2, 3) must_== 5
-      }
+      "b" in { code(1, 2) must_== 3 }
+      "c" in { code(2, 3) must_== 5 }
     }
   }
-
 }
-
