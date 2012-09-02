72b11095ff2ddb05bbe33feb126398fc6dc7bc81
njalldev <narciso.benigno@gmail.com>
2012-09-02 01:28:23 -0300

    removendo back url, usando flash scoped

--- src/main/scala/vggames/shared/auth/AuthController.scala
+++ src/main/scala/vggames/shared/auth/AuthController.scala
@@ -4,23 +4,33 @@ import br.com.caelum.vraptor.{Result, Resource, Get}
 import vggames.shared.UserHost
 
 @Resource
-class AuthController(result : Result, viseted : BackUrl, authenticate: StartAuthentication) {
+class AuthController(result : Result, authenticate: StartAuthentication) {
 
   @Get(Array("/auth/provider/{provider}"))
   def provider(provider : String, backUrl : String) {
-    viseted.value = backUrl
-    // TODO: Needs Persist the AuthenticateWithProvider
-    result.redirectTo(authenticate.withA(provider).authorizationUrl)
+    result.include(backUrlParamName, backUrl)
+    result.include(authenticationParamName, authenticate.withA(provider))
+    result.redirectTo(authenticationWith.authorizationUrl)
   }
 
   @Get(Array("/authorization/provider/{provider}"))
   def authorization(provider: String, oauth_token : String, oauth_verifier : String) {
-//    player.authorize(AuthorizationVerifier(oauth_verifier))
-    result.redirectTo(viseted.value)
+    result.redirectTo(backUrl)
   }
+
+
 
   def logout {
 //    player.logout
     result.redirectTo(classOf[UserHost]).home;
   }
+
+  private def backUrl: String = result.included().get(backUrlParamName).toString
+
+  private def backUrlParamName = "backUrl"
+
+  private def authenticationWith = result.included().get(authenticationParamName).asInstanceOf[AuthenticateWithProvider]
+
+  private def authenticationParamName = "authenticationWith"
+
 }
--- src/main/scala/vggames/shared/auth/BackUrl.scala
+++
@@ -1,9 +0,0 @@
-package vggames.shared.auth
-
-import br.com.caelum.vraptor.ioc.{SessionScoped, Component}
-
-@Component
-@SessionScoped
-class BackUrl {
-  var value:String = _ 
-}
