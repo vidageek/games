5399c326fd2b60710d2350c23bfa7770310efddb
Jonas Abreu <jonas@vidageek.net>
2012-09-26 01:20:29 -0300

    fazendo backup antes de fazer o deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 
+echo "Fazendo backup do WAR"
+ssh -i ~/.ssh/games.pem ubuntu@177.71.178.115 && cp ~/jetty/webapps/games.war ~/games.war.bkp
+
 ssh -i /private/vidageek/games.pem ubuntu@177.71.178.115 "/bin/bash ~/jetty/bin/jetty.sh stop && cp ~/games.war ~/jetty/webapps/. && /bin/bash ~/jetty/bin/jetty.sh start"
 
