a26003ef34c433f7e1dc84a051f0c149f167783f
Jonas Abreu <jonas@vidageek.net>
2012-09-27 22:26:49 -0300

    removendo envio de email do watchdog, pois a própria aplicação enviará.

--- watchdog.sh
+++ watchdog.sh
@@ -9,8 +9,12 @@ echo -n "$(date +%Y%m%d-%H:%M:%S) Verificando se games responde: "
 
 if [ -z "$(curl --connect-timeout $TIMEOUT -m $TIMEOUT -vv http://$HOST$URL_PATH 2>&1 | grep "200 OK")" ]; then 
 	echo "Fail! Rebooting!"
-	mail -s "[GamesWatchDog] Application Reboot at $(date +%Y%m%d-%H:%M:%S)" jonasabr@gmail.com < /dev/null
+	
+	$JETTY_HOME/bin/jetty.sh stop
+	
+	sleep 3
 	kill -9 $(jps -l | grep start.jar | cut -d " " -f 1)
+	
 	$JETTY_HOME/bin/jetty.sh start
 else
 	echo "Ok!"
