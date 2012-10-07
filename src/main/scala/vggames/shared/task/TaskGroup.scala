2971f44319b495b585a2c42f3370428361934ff4
Jonas Abreu <jonas@vidageek.net>
2012-10-07 19:54:33 -0300

    removendo método que não era mais chamado

--- src/main/scala/vggames/shared/task/TaskGroup.scala
+++ src/main/scala/vggames/shared/task/TaskGroup.scala
@@ -2,8 +2,6 @@ package vggames.shared.task
 
 class TaskGroup(name : String, groupName : String, descriptions : Descriptions, tasks : Task*) {
 
-  def task(index : Int) = tasks(index)
-
   def getDescription = descriptions.forGroup(groupName)
 
   def getName = name
