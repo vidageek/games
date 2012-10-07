94dec4bf8e84ab04a489ddc8c996a3d781e31794
Jonas Abreu <jonas@vidageek.net>
2012-10-07 19:45:48 -0300

    removendo métodos que não eram usados

--- src/main/scala/vggames/shared/task/TaskGroup.scala
+++ src/main/scala/vggames/shared/task/TaskGroup.scala
@@ -2,11 +2,7 @@ package vggames.shared.task
 
 class TaskGroup(name : String, groupName : String, descriptions : Descriptions, tasks : Task*) {
 
-  def size = tasks.size
-
   def task(index : Int) = tasks(index)
-
-  def iterator = tasks.iterator
 
   def getDescription = descriptions.forGroup(groupName)
 
