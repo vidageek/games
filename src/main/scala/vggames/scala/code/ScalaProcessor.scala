c1382acc627fdaeb68c6830c723b53c251766c22
Jonas Abreu <jonas@vidageek.net>
2012-10-19 17:58:38 -0300

    tentativa de melhorar performance do compilador scala. 60% mais rápido mas com falhas intermitentes.

--- src/main/scala/vggames/scala/code/ScalaProcessor.scala
+++ src/main/scala/vggames/scala/code/ScalaProcessor.scala
@@ -1,24 +1,29 @@
 package vggames.scala.code
 
-import com.twitter.util.Eval
+import com.twitter.util.{ Eval => TwitterEval }
 import java.security.Permission
 import vggames.scala.specs.GameSpecification
 import vggames.scala.tasks.judge.ExecutionFailure
 import vggames.shared.task.JudgedTask
 import vggames.scala.code.Wrappers._
+import scala.collection.mutable.Queue
+import scala.collection.mutable.ListBuffer
+import java.io.File
+import java.security.MessageDigest
+import scala.math.BigInt
 
 class ScalaProcessor[T <: CodeRestrictions[_]](spec : GameSpecification[T]) {
   val className = "ExpressionRunner"
   val fullName = "scalagameunsafe.ExpressionRunner"
 
   def processCode(code : String) : JudgedTask = {
-    val eval = new Eval(None)
-    compile(code, eval)
-    run(className, eval, code)
+
+    val klass = compile(code)
+    run(className, klass, code)
   }
 
-  def run(className : String, eval : Eval, submittedCode : String) : JudgedTask = {
-    val code = eval.findClass(fullName).newInstance.asInstanceOf[T]
+  def run(className : String, klass : Class[_], submittedCode : String) : JudgedTask = {
+    val code = klass.newInstance.asInstanceOf[T]
     try {
       spec.run(code, submittedCode).judgement
     } catch {
@@ -26,12 +31,44 @@ class ScalaProcessor[T <: CodeRestrictions[_]](spec : GameSpecification[T]) {
     }
   }
 
-  private def compile(code : String, eval : Eval) = {
+  def hash(s : String) =
+    BigInt(MessageDigest.getInstance("MD5").digest(s.getBytes)).toString(16).replace("-", "")
+
+  private def compile(code : String) = {
     if (code.contains("finally") || code.contains("catch") ||
       code.contains(TaskRunSecurityManager.getClass.getSimpleName.replace("$", "")))
       throw new SecurityException("Tentativa de executar c&oacute;digo privilegiado dentro de uma task.")
-    val wrapped = wrap(className, code, spec.extendsType, spec.runSignature, spec.afterCode)
-    eval.compile(wrapped)
+    val codeHash = hash(code)
+    val wrapped = wrap(className + codeHash, code, spec.extendsType, spec.runSignature, spec.afterCode)
+    Compile(_.toClass(wrapped, fullName + codeHash))
+  }
+}
+
+object Compile {
+  val pool = ListBuffer[Eval]()
+
+  def compiler() = synchronized { pool.headOption.getOrElse(new Eval(None)) }
+
+  def discard(eval : Eval) = synchronized { pool += eval }
+
+  def apply(f : Eval => Class[_]) = {
+    val eval = compiler()
+    try {
+      f(eval)
+    } catch {
+      case t => { t.printStackTrace; throw t }
+
+    } finally {
+      discard(eval)
+    }
+  }
+}
+
+class Eval(file : Option[File]) extends TwitterEval(file) {
+
+  def toClass(code : String, name : String) = {
+    compile(code)
+    findClass(name)
   }
 }
 
