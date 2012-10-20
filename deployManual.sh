cfcfe852a5e883fe06414fcc42bbbfd0665cb52f
Jonas Abreu <jonas@vidageek.net>
2012-10-20 18:11:48 -0300

    corrigido script de deploy. Força o rebuild do war para evitar problemas com a compilação incremental do sbt. (em alguns casos tem gerado bytecode inválido...)

--- deployManual.sh
+++ deployManual.sh
@@ -1,20 +1,24 @@
 #! /bin/bash
 
+echo "Rebuilding war para evitar Bad Build (sbt incremental falha algumas vezes)"
+rm -fr target
+./s gzip-css gzip-js package
+
 echo "Fazendo backup do WAR"
 ssh -i ~/.ssh/games.pem ubuntu@177.71.178.115 "cp ~/jetty/webapps/games.war ~/games.war.bkp"
 
-echo "Copiando o War"
+echo "Copiando o War para servidor remoto"
 scp -i ~/.ssh/games.pem target/scala-2.9.2/games* ubuntu@177.71.178.115:~/games.war
 
 DEPLOY="echo Parando Jetty"
-DEPLOY=$DEPLOY + " && /bin/bash ~/jetty/bin/jetty.sh stop"
-DEPLOY=$DEPLOY + " && echo Fazendo backup do Banco"
-DEPLOY=$DEPLOY + " && cp ~/jetty/games.db ~/games.db.bkp"
-DEPLOY=$DEPLOY + " && echo Copiando war"
-DEPLOY=$DEPLOY + " && cp ~/games.war ~/jetty/webapps/."
-DEPLOY=$DEPLOY + " && echo Subindo Jetty"
-DEPLOY=$DEPLOY + " && /bin/bash ~/jetty/bin/jetty.sh start"
-DEPLOY=$DEPLOY + " && echo Deploy Concluído"
+DEPLOY="$DEPLOY && /bin/bash ~/jetty/bin/jetty.sh stop"
+DEPLOY="$DEPLOY && echo Fazendo backup do Banco"
+DEPLOY="$DEPLOY && cp ~/jetty/games.db ~/games.db.bkp"
+DEPLOY="$DEPLOY && echo Copiando war"
+DEPLOY="$DEPLOY && cp ~/games.war ~/jetty/webapps/."
+DEPLOY="$DEPLOY && echo Subindo Jetty"
+DEPLOY="$DEPLOY && /bin/bash ~/jetty/bin/jetty.sh start"
+DEPLOY="$DEPLOY && echo Deploy Concluído"
 
 ssh -i ~/.ssh/games.pem ubuntu@177.71.178.115 "$DEPLOY"
 
