de3e316cc2a22414332c9b1c72352f1f5a5b89a2
Jonas Abreu <jonas@vidageek.net>
2012-10-22 16:06:04 -0200

    corrigindo bug que plugin do eclipse não consegue compilar, mas sbt vai tranquilo o.O

--- src/test/scala/vggames/regex/task/FailedsSpec.scala
+++ src/test/scala/vggames/regex/task/FailedsSpec.scala
@@ -14,13 +14,13 @@ class FailedsSpec extends Specification {
     faileds.addOnlyJudgedFailed(new Failed("[aPattern1] não dá match em [matchingTarget1]"))
 
     "show description to any faileds" in {
-      faileds.mkString("<br />") must_== faileds.reason()
+      faileds.mkString("<br />") must_== faileds.reason
     }
 
     "show description to all faileds" in {
       val testFaileds = new Faileds()
       testFaileds.addAll(faileds)
-      testFaileds.mkString("<br />") must_== testFaileds.reason()
+      testFaileds.mkString("<br />") must_== testFaileds.reason
     }
   }
 }
