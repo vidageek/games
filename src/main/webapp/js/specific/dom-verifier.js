d430b6f262728b0101f66f061b9323a244e5ad7d
Jonas Abreu <jonas@vidageek.net>
2012-10-31 23:00:59 -0200

    validação de estrutura funcionando. closes #139

--- src/main/webapp/js/specific/dom-verifier.js
+++ src/main/webapp/js/specific/dom-verifier.js
@@ -1,4 +1,42 @@
-function verify(reference, challenge) {
+function verify(reference, challenge, challengeString) {
+	return verifySimilarity(reference, challenge).concat(verifyWellFormedNess(challengeString));
+}
+
+function verifyWellFormedNess(challenge) {
+	var voidTags = /area|base|br|col|command|embed|hr|img|input|keygen|link|meta|param|source|track|wbr/i;
+	var tagPattern = /<(\/?[^ \>\/]+)/gm;
+	var stack = [];
+	var errors = [];
+	var tag = tagPattern.exec(challenge);
+	while (tag) {
+		tag = tag[1]
+		if (tag.charAt(0) == "/") {
+			var top = last(stack);
+			if (top == tag.substring(1)){
+				stack.pop();
+			} else {
+				errors.push("Encontrada tag não fechada " + top);
+			}
+		} else {
+			if (!voidTags.test(tag)) {
+				stack.push(tag);			
+			}
+		}
+		// challenge = challenge.substring(challenge);
+		tag = tagPattern.exec(challenge);
+	}
+	while(stack.length > 0) {
+		errors.push("Encontrada tag não fechada " + stack.pop());
+		console.log(2);
+	}
+	return errors;
+}
+
+function last(array) {
+	return array[array.length -1];
+}
+
+function verifySimilarity(reference, challenge) {
 	if (!reference && !challenge){
 		return [];
 	}
@@ -28,8 +66,8 @@ function verify(reference, challenge) {
 	}
 	
 	var errors = [];
-	for (var i = 0; i < referenceChildren.length; i++) {
-		var res = verify(referenceChildren[i], childrenOrNull(challengeChildren, i))
+	for (var i = 0; i < Math.max(referenceChildren.length, challengeChildren.length); i++) {
+		var res = verifySimilarity(childrenOrNull(referenceChildren[i]), childrenOrNull(challengeChildren, i))
 		errors = errors.concat(res);
 	}
 	return errors;
--- src/main/webapp/js/specific/html-start.js
+++ src/main/webapp/js/specific/html-start.js
@@ -10,12 +10,12 @@ $(document).ready(function(){
 	  mode: "text/html"
 	});
 	
-	window.setInterval(function() {
+	function checkCode() {
 		var value = editor.getValue();
 		if (!(value == code)) {
 			code = value;
 			var challenge = $('#render-challenge').contents().find("body").html(code);
-			var errors = verify($('#render-answer').contents().find("body")[0], challenge[0]);
+			var errors = verify($('#render-answer').contents().find("body")[0], challenge[0], code);
 			if (errors.length > 0){
 				var errorHtml = "<ul>" + $.map(errors, function(e){return "<li>" + e + "</li>"}).join("") + "</ul>"
 				$('#challenge-result').removeClass("alert-success").addClass("reason alert alert-error").html(errorHtml);
@@ -23,5 +23,7 @@ $(document).ready(function(){
 				$('#challenge-result').removeClass("alert-error").addClass("alert-success").html("Ok!");
 			}
 		}
-	} ,100);
+		window.setTimeout(checkCode, 100);
+	}
+	window.setTimeout(checkCode ,100);
 });
