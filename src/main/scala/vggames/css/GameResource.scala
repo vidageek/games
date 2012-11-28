03d3584c3507359e1e6cc46c56cdb7fe8b0d1612
njalldev <narciso.benigno@gmail.com>
2012-11-28 01:25:17 -0200

    css e fragment para css game

---
+++ src/main/scala/vggames/css/GameResource.scala
@@ -0,0 +1,27 @@
+package vggames.css
+
+import br.com.caelum.vraptor.Result
+import javax.servlet.http.HttpServletResponse
+import br.com.caelum.vraptor.View
+import br.com.caelum.vraptor.ioc.Component
+import scala.io.Source
+import br.com.caelum.vraptor.Resource
+import br.com.caelum.vraptor.Get
+
+@Resource
+class GameResource(result: Result) {
+
+  @Get(Array("/play/css/resource/{resource}"))
+  def gameCss(resource: String) =
+    result.use(classOf[Css]).from(resource)
+    
+}
+
+@Component
+class Css(res: HttpServletResponse) extends View {
+  def from(resource: String) {
+    res.setContentType("text/css")
+    res.getWriter().println(
+      Source.fromInputStream(getClass.getResourceAsStream("/css/%s.css".format(resource))).mkString)
+  }
+} 
