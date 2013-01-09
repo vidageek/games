65af52528f95618d4e7f22c634175c3e337cdc23
Jonas Abreu <jonas@vidageek.net>
2013-01-09 16:12:11 -0200

    renomenando arquivos para syntax highlight funcionar automaticamente (porque os arquivos são markdown agora)

--- src/main/scala/vggames/shared/task/Descriptions.scala
+++ src/main/scala/vggames/shared/task/Descriptions.scala
@@ -12,7 +12,7 @@ class Descriptions(game : String) {
 
   def forGroup(groupName : String) : String = {
     descriptions.get(groupName).getOrElse {
-      val desc = Option(classOf[Descriptions].getResourceAsStream("/desc/" + game + "/" + groupName + ".html")).
+      val desc = Option(classOf[Descriptions].getResourceAsStream("/desc/" + game + "/" + groupName + ".markdown")).
         map(new Scanner(_).useDelimiter("$$").next).map(new Markdown()(_)).getOrElse("No description for group " + groupName)
       descriptions.put(groupName, desc)
       desc
--- src/test/scala/vggames/invariant/DescriptionInvariantSpec.scala
+++ src/test/scala/vggames/invariant/DescriptionInvariantSpec.scala
@@ -1,22 +1,14 @@
 package vggames.invariant
+
 import java.util.regex.Pattern
-import net.vidageek.invariant._
-import org.junit.Assert._
+
+import org.junit.Assert.assertTrue
 import org.junit.runner.RunWith
-import net.vidageek.invariant.Invariant
-import net.vidageek.invariant.InvariantRunner
-import org.junit.runner.RunWith
+
+import net.vidageek.invariant.{FileData, Invariant, InvariantRunner}
 
 @RunWith(classOf[InvariantRunner])
 class DescriptionInvariantTest {
-  val ALLOWED_CHARS = "[\\w\\s\\Q<>&/.,();+=^:\\?[]-${}!|*\"'\\E]*"
-
-  @Invariant(affects = ".*\\.html", folder = "src/main/resources")
-  def allDescriptionsMustUseHtmlEntities(data : FileData) = {
-    val content = data.getContent()
-    assertFalse("Found invalid chars: [" + content.replaceAll(ALLOWED_CHARS, "") + "]",
-      containsInvalidChars(content))
-  }
 
   @Invariant(affects = ".*\\.html", folder = "src/main/resources")
   def regexMustBeProperlyWritten(data : FileData) = {
@@ -25,6 +17,4 @@ class DescriptionInvariantTest {
       assertTrue("Found invalid way of writing RegEx", "RegEx".equals(matcher.group(1)))
     }
   }
-
-  def containsInvalidChars(content : String) = !content.matches(ALLOWED_CHARS)
 }
