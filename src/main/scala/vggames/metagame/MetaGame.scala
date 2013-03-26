94495b0a53f8a5d97b4df828cf018559f680ea88
Caio Valente <caio99valente@hotmail.com>
2013-03-26 14:28:19 -0300

    Consertando build
    Conserto na classe MetaGame para compilar.

--- src/main/scala/vggames/metagame/MetaGame.scala
+++ src/main/scala/vggames/metagame/MetaGame.scala
@@ -2,9 +2,12 @@ package vggames.metagame
 
 import vggames.shared.Game
 import vggames.shared.task.Descriptions
+import vggames.shared.task.Tasks
 
 class MetaGame (descriptions : Descriptions) extends Game {
-
+  override val tasks = new Tasks(
+    )
+  
   def getDescription(): String = { null }
 
   def getName(): String = { null }
