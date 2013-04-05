7fd6f886a132ef18195884d3b9d1b20cf32f23f6
Jonas Abreu <jonas@vidageek.net>
2013-04-05 13:08:24 -0300

    adicionado teste para evitar uso de html nos arquivos markdown

--- src/test/scala/vggames/invariant/DescriptionInvariantSpec.scala
+++ src/test/scala/vggames/invariant/DescriptionInvariantSpec.scala
@@ -1,20 +1,24 @@
 package vggames.invariant
 
 import java.util.regex.Pattern
-
-import org.junit.Assert.assertTrue
+import org.junit.Assert.{ assertTrue, assertFalse }
 import org.junit.runner.RunWith
-
-import net.vidageek.invariant.{FileData, Invariant, InvariantRunner}
+import net.vidageek.invariant.{ FileData, Invariant, InvariantRunner }
 
 @RunWith(classOf[InvariantRunner])
 class DescriptionInvariantTest {
 
-  @Invariant(affects = ".*\\.html", folder = "src/main/resources")
+  @Invariant(affects = ".*\\.markdown", folder = "src/main/resources/desc")
   def regexMustBeProperlyWritten(data : FileData) = {
     val matcher = Pattern.compile("(?i)(regex)[^\"]").matcher(data.getContent())
     while (matcher.find()) {
       assertTrue("Found invalid way of writing RegEx", "RegEx".equals(matcher.group(1)))
     }
+  }
+
+  @Invariant(affects = ".*\\.markdown", folder = "src/main/resources/desc")
+  def mustNotContainHtmlEntities(data : FileData) = {
+    val matcher = Pattern.compile("&[^;]{0,10};").matcher(data.getContent())
+    assertFalse("Found use of html entity on markdown file", matcher.find())
   }
 }
