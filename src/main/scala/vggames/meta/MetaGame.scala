adad5301693b542991108a8efd7019cea0bc5565
Miguel Angel Galarreta V <miguelg@ime.usp.br>
2013-04-09 15:11:50 -0300

    adicionado o registro na factory no metagames

--- src/main/scala/vggames/meta/MetaGame.scala
+++ src/main/scala/vggames/meta/MetaGame.scala
@@ -9,16 +9,22 @@ class MetaGame (descriptions : Descriptions) extends Game {
   override val tasks = new Tasks(
       firstTaskGroup,
       secondTaskGroup,
+      registerFactoryGroup,
       test
     )
 
   private def firstTaskGroup = new TaskGroup("Primeiro Grupo de Tarefas", "meta.first", descriptions,
     new MetaTask("Digite o nome do seu jogo"),
-    new MetaTask("Digite o nome de um outro jogo"));
+    new MetaTask("Digite o nome de um outro jogo"))
 
   private def secondTaskGroup = new TaskGroup("Segundo Grupo de Tarefas", "meta.second", descriptions,
     new MetaTask("Digite o nome do seu jogo"),
-    new MetaTask("Digite o nome de um outro jogo"));
+    new MetaTask("Digite o nome de um outro jogo"))
+  
+  private def registerFactoryGroup = new TaskGroup("Registro do jogo na classe factory", "meta.register", descriptions,   
+    new MetaTask("Procurar a classe GameFactory.scala"),
+  	new MetaTask("Registrar sua classe na factory")  
+  )
   
   private def test = new TaskGroup("Criação de testes para um novo game", "meta.test", descriptions,
       new MetaTask("Seguindo o exemplo dos testes existentes para o MathGame crie uma classe de teste para seu jogo.")
