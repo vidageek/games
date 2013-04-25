120cdfb3324fb9211750dcabd64ebcb461c48ea7
Felipe Amado & Miguel Angel Galarreta V <miguelg@ime.usp.br>
2013-04-25 16:01:10 -0300

    Trocando body por html

--- src/main/webapp/js/specific/html-start.js
+++ src/main/webapp/js/specific/html-start.js
@@ -15,8 +15,8 @@ $(document).ready(function(){
 		var value = editor.getValue();
 		if (!(value == code)) {
 			code = value;
-			var challenge = $('#render-challenge').contents().find("body").html(code);
-			var errors = verify($('#render-answer').contents().find("body")[0], challenge[0], code);
+			var challenge = $('#render-challenge').contents().find("html").html(code);
+			var errors = verify($('#render-answer').contents().find("html")[0], challenge[0], code);
 			if (errors.length > 0){
 				var errorHtml = "<ul>" + $.map(errors, function(e){return "<li>" + e + "</li>"}).join("") + "</ul>"
 				$('#challenge-result').removeClass("alert-success").addClass("reason alert alert-error").html(errorHtml);
