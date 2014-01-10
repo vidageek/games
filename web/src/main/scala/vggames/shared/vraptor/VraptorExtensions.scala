d422268cad763c74d82b2bbc6469be3f66d6da10
Jonas Abreu <jonas@vidageek.net>
2014-01-10 19:23:14 -0200

    colocando game no request a cada requisicao

--- web/src/main/scala/vggames/shared/vraptor/VraptorExtensions.scala
+++ web/src/main/scala/vggames/shared/vraptor/VraptorExtensions.scala
@@ -8,6 +8,11 @@ import br.com.caelum.vraptor.View
 import javax.servlet.http.HttpServletResponse
 import java.nio.charset.Charset
 import java.io.PrintWriter
+import br.com.caelum.vraptor.Intercepts
+import javax.servlet.http.HttpServletRequest
+import br.com.caelum.vraptor.interceptor.Interceptor
+import br.com.caelum.vraptor.core.InterceptorStack
+import br.com.caelum.vraptor.resource.ResourceMethod
 
 object VraptorExtensions {
 
@@ -26,5 +31,20 @@ class GameView(response : HttpServletResponse) extends View {
     response.setCharacterEncoding("UTF-8")
     response.getOutputStream().write(html.getBytes(Charset.forName("UTF-8")))
   }
+}
+
+@Intercepts
+class AddGameInterceptor(req : HttpServletRequest, data : RequestData, cached : GameFactoryCache) extends Interceptor {
+
+  override def intercept(stack : InterceptorStack, method : ResourceMethod, resourceInstance : Any) {
+    if (!"".equals(data.game)) {
+      req.setAttribute("gameName", data.game)
+      req.setAttribute("game", cached(data.game).getOrElse(""))
+
+    }
+    stack.next(method, resourceInstance)
+  }
+
+  override def accepts(method : ResourceMethod) = true
 
 }
