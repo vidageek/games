df83e65777d51caa3891be99f3bb9759c4c05061
Jonas Abreu <jonas@vidageek.net>
2014-01-12 11:49:26 -0200

    view do meta game migrada

---
+++ games/meta/src/main/scala/vggames/meta/MetaGameView.scala
@@ -0,0 +1,22 @@
+package vggames.meta
+
+import vggames.shared.GameView
+import vggames.shared.Game
+import vggames.shared.task.TaskWithDescription
+import vggames.shared.task.JudgedTask
+import scalatags._
+
+class MetaGameView extends GameView {
+
+  def render(game : Game, task : TaskWithDescription, judgedTask : Option[JudgedTask], lastAttempt : String) = {
+
+    div("row".cls)(
+      div("span5".cls)(
+        judgement(judgedTask),
+        form("challenge".cls, "method".attr := "POST", action := s"/play/${game.path}/task/${task.getIndex}")(
+          label("for".attr := "challenge")(strong(raw(task.getChallenge))),
+          input(id := "challenge-submit", "btn btn-primary".cls, "type".attr := "submit", value := "Check!")),
+        progressBar(task, game)),
+      taskDescription(task, "span7")).toString
+  }
+}
