bae33d38a4c099d45222b461f93548b0566bd142
jcranky <paulo.siqueira@gmail.com>
2014-01-13 02:45:01 -0200

    Atualiza diversas libs.

--- project/GamesVidageekBuild.scala
+++ project/GamesVidageekBuild.scala
@@ -48,7 +48,7 @@ object GamesVidageekBuild extends Build {
   lazy val commonSettings: Seq[Setting[_]] = Defaults.defaultSettings ++ Seq(
     organization := "net.vidageek",
     version := "0.1-SNAPSHOT",
-    scalaVersion := "2.10.0",
+    scalaVersion := "2.10.3",
     scalacOptions ++= Seq("-encoding", "UTF-8", "-deprecation", "-unchecked", "-g:vars", "-feature",
       "-language:_"),
     libraryDependencies ++= Seq(junit, specs2, mockito, junitInterface, scalaTags))
@@ -82,16 +82,16 @@ object GamesVidageekBuild extends Build {
     lazy val log4j = "log4j" % "log4j" % "1.2.16"
     lazy val velocity = "org.apache.velocity" % "velocity" % "1.7"
     lazy val jstl = "javax.servlet" % "jstl" % "1.2"
-    lazy val guice = "com.google.inject" % "guice" % "3.0-rc2"
-    lazy val guiceBindings = "com.google.inject.extensions" % "guice-multibindings" % "3.0-rc2"
+    lazy val guice = "com.google.inject" % "guice" % "3.0"
+    lazy val guiceBindings = "com.google.inject.extensions" % "guice-multibindings" % "3.0"
     lazy val servletApi = "javax.servlet" % "servlet-api" % "2.5" % "provided"
     lazy val jspApi = "javax.servlet.jsp" % "jsp-api" % "2.1" % "provided"
-    lazy val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.1.4"
+    lazy val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.2.3"
     lazy val slick = "com.typesafe.slick" %% "slick" % "1.0.0"
-    lazy val scalaReflect = "org.scala-lang" % "scala-reflect" % "2.10.0"
-    lazy val scalaCompiler = "org.scala-lang" % "scala-compiler" % "2.10.0"
+    lazy val scalaReflect = "org.scala-lang" % "scala-reflect" % "2.10.3"
+    lazy val scalaCompiler = "org.scala-lang" % "scala-compiler" % "2.10.3"
     lazy val sqlite = "org.xerial" % "sqlite-jdbc" % "3.7.2"
-    lazy val aws = "com.amazonaws" % "aws-java-sdk" % "1.3.20"
+    lazy val aws = "com.amazonaws" % "aws-java-sdk" % "1.6.12"
     lazy val actuarius = "eu.henkelmann" % "actuarius_2.10.0" % "0.2.5"
     lazy val sitemesh = "opensymphony" % "sitemesh" % "2.4.2"
     lazy val scalaTags = "com.scalatags" % "scalatags_2.10" % "0.2.0"
@@ -106,7 +106,7 @@ object GamesVidageekBuild extends Build {
     lazy val selenium = "org.seleniumhq.selenium" % "selenium-firefox-driver" % "2.32.0" % "test"
     lazy val mockito = "org.mockito" % "mockito-core" % "1.9.0" % "test"
     lazy val junit = "junit" % "junit" % "4.10" % "test"
-    lazy val akkaTestkit = "com.typesafe.akka" %% "akka-testkit" % "2.1.1" % "test"
+    lazy val akkaTestkit = "com.typesafe.akka" %% "akka-testkit" % "2.2.3" % "test"
     lazy val junitInterface = "com.novocode" % "junit-interface" % "0.8" % "test"
   }
 }
