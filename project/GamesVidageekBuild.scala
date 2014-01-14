0ed147313fbb2c8bd107b1e926461c39777249c3
jcranky <paulo.siqueira@gmail.com>
2014-01-13 22:46:25 -0200

    Remove 'lazy' dos vals de dependencias.

--- project/GamesVidageekBuild.scala
+++ project/GamesVidageekBuild.scala
@@ -1,3 +1,4 @@
+
 import java.io.{ FileOutputStream, FileInputStream, PrintWriter }
 import java.util.Scanner
 import java.util.zip.{ GZIPOutputStream, GZIPInputStream }
@@ -14,7 +15,7 @@ import com.gu.SbtJasminePlugin._
 object GamesVidageekBuild extends Build {
   import Dependencies._
   import TestDependencies._
-  
+
   seq(jasmineSettings: _*)
 
   lazy val root = Project(
@@ -74,39 +75,38 @@ object GamesVidageekBuild extends Build {
     dependsOn(game)
 
   object Dependencies {
-    lazy val jettyWebapp = "org.eclipse.jetty" % "jetty-webapp" % jettyVersion % "container"
-    lazy val jettyServlets = "org.eclipse.jetty" % "jetty-servlets" % jettyVersion % "container"
-    lazy val jettyJsp = "org.eclipse.jetty" % "jetty-jsp-2.1" % jettyVersion % "container"
-    lazy val jsp = "org.mortbay.jetty" % "jsp-2.1-glassfish" % "2.1.v20100127" % "container"
-    lazy val xstream = "com.thoughtworks.xstream" % "xstream" % "1.4.2"
-    lazy val log4j = "log4j" % "log4j" % "1.2.16"
-    lazy val velocity = "org.apache.velocity" % "velocity" % "1.7"
-    lazy val jstl = "javax.servlet" % "jstl" % "1.2"
-    lazy val guice = "com.google.inject" % "guice" % "3.0"
-    lazy val guiceBindings = "com.google.inject.extensions" % "guice-multibindings" % "3.0"
-    lazy val servletApi = "javax.servlet" % "servlet-api" % "2.5" % "provided"
-    lazy val jspApi = "javax.servlet.jsp" % "jsp-api" % "2.1" % "provided"
-    lazy val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.2.3"
-    lazy val slick = "com.typesafe.slick" %% "slick" % "1.0.0"
-    lazy val scalaReflect = "org.scala-lang" % "scala-reflect" % "2.10.3"
-    lazy val scalaCompiler = "org.scala-lang" % "scala-compiler" % "2.10.3"
-    lazy val sqlite = "org.xerial" % "sqlite-jdbc" % "3.7.2"
-    lazy val aws = "com.amazonaws" % "aws-java-sdk" % "1.6.12"
-    lazy val actuarius = "eu.henkelmann" % "actuarius_2.10.0" % "0.2.5"
-    lazy val sitemesh = "opensymphony" % "sitemesh" % "2.4.2"
-    lazy val scalaTags = "com.scalatags" % "scalatags_2.10" % "0.2.0"
-    lazy val specs2 = "org.specs2" %% "specs2" % "2.3.7"
+    val jettyVersion = "7.4.5.v20110725"
 
-    lazy val vraptor = "br.com.caelum" % "vraptor" % "3.5.3" excludeAll (ExclusionRule(organization = "org.springframework"))
-
-    lazy val jettyVersion = "7.4.5.v20110725"
+    val jettyWebapp = "org.eclipse.jetty" % "jetty-webapp" % jettyVersion % "container"
+    val jettyServlets = "org.eclipse.jetty" % "jetty-servlets" % jettyVersion % "container"
+    val jettyJsp = "org.eclipse.jetty" % "jetty-jsp-2.1" % jettyVersion % "container"
+    val jsp = "org.mortbay.jetty" % "jsp-2.1-glassfish" % "2.1.v20100127" % "container"
+    val xstream = "com.thoughtworks.xstream" % "xstream" % "1.4.2"
+    val log4j = "log4j" % "log4j" % "1.2.16"
+    val velocity = "org.apache.velocity" % "velocity" % "1.7"
+    val jstl = "javax.servlet" % "jstl" % "1.2"
+    val guice = "com.google.inject" % "guice" % "3.0"
+    val guiceBindings = "com.google.inject.extensions" % "guice-multibindings" % "3.0"
+    val servletApi = "javax.servlet" % "servlet-api" % "2.5" % "provided"
+    val jspApi = "javax.servlet.jsp" % "jsp-api" % "2.1" % "provided"
+    val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.2.3"
+    val slick = "com.typesafe.slick" %% "slick" % "1.0.0"
+    val scalaReflect = "org.scala-lang" % "scala-reflect" % "2.10.3"
+    val scalaCompiler = "org.scala-lang" % "scala-compiler" % "2.10.3"
+    val sqlite = "org.xerial" % "sqlite-jdbc" % "3.7.2"
+    val aws = "com.amazonaws" % "aws-java-sdk" % "1.6.12"
+    val actuarius = "eu.henkelmann" % "actuarius_2.10.0" % "0.2.6"
+    val sitemesh = "opensymphony" % "sitemesh" % "2.4.2"
+    val scalaTags = "com.scalatags" % "scalatags_2.10" % "0.2.0"
+    val specs2 = "org.specs2" %% "specs2" % "2.3.7"
+    val vraptor = "br.com.caelum" % "vraptor" % "3.5.3" excludeAll (ExclusionRule(organization = "org.springframework"))
   }
 
   object TestDependencies {
-    lazy val selenium = "org.seleniumhq.selenium" % "selenium-firefox-driver" % "2.32.0" % "test"
-    lazy val mockito = "org.mockito" % "mockito-core" % "1.9.0" % "test"
-    lazy val junit = "junit" % "junit" % "4.10" % "test"
-    lazy val akkaTestkit = "com.typesafe.akka" %% "akka-testkit" % "2.2.3" % "test"
-    lazy val junitInterface = "com.novocode" % "junit-interface" % "0.8" % "test"
+    val selenium = "org.seleniumhq.selenium" % "selenium-firefox-driver" % "2.32.0" % "test"
+    val mockito = "org.mockito" % "mockito-core" % "1.9.0" % "test"
+    val junit = "junit" % "junit" % "4.10" % "test"
+    val akkaTestkit = "com.typesafe.akka" %% "akka-testkit" % "2.2.3" % "test"
+    val junitInterface = "com.novocode" % "junit-interface" % "0.8" % "test"
   }
 }
