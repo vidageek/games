0bcd7fcf9668b89b963922cc237c2a0c72f94172
Jonas Abreu <jonas@vidageek.net>
2014-10-27 17:11:49 -0100

    improved html game description

--- games/html/src/main/scala/vggames/html/HtmlGame.scala
+++ games/html/src/main/scala/vggames/html/HtmlGame.scala
@@ -111,7 +111,20 @@ class HtmlGame extends GameEngine {
   // script
   private def includeJs = TaskGroup("Incluir Js", "include.js")
 
-  def description = "Html Game"
+  def description = """Html é uma linguagem que foi criada para acrescentarmos informações à texto de forma que um computador seja capaz de entender o significado das estruturas do texto.
+
+Isso é necessário porque, quando olhamos para textos como:
+
+  Comprar:
+      - batata
+      - cenouras
+      - frutas
+
+nós sabemos que isso é uma lista, mas um computador não sabe. E como ele não sabe, ele não consegue automatizar tarefas para nós (imagine a web sem links, você tendo que copiar a colar todas as urls que quiser visitar).
+
+Hoje isso vai muito mais longe. Ferramentas como buscadores e outros analizam as nossas páginas para identificar, relacionar e tentar apresentar o melhor resultado para perguntas. Sem essa informação, isso seria praticamente impossível.
+
+Esse jogo cobre as principais tags e atributos de Html 5"""
 
   def name = "Html"
 
