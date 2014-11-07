03642df238e9ba46a32e29beef1d6ab11a32d819
Jonas Abreu <jonas@vidageek.net>
2014-11-07 11:13:53 -0200

    changing build script to use the deploy key

--- build.sh
+++ build.sh
@@ -1,24 +1,12 @@
 #! /bin/bash
 
-SBT_JAR=/opt/sbt/sbt-launch-0.13.0.jar
-
-SBT="/opt/jdk/jdk1.7.latest/bin/java -XX:PermSize=372m -Dsbt.log.noformat=true -jar $SBT_JAR"
-
-BUILD=0
-
 set -e
 
-rm -fr target
-
-$SBT compile
-
-$SBT test
-
-$SBT package
+sbt test package
 
 echo "Fazendo backup do WAR"
-ssh -i /private/vidageek/games.pem ubuntu@177.71.178.115 "cp ~/jetty/webapps/games.war ~/games.war.bkp"
+ssh -i keys/travis_deploy ubuntu@games.vidageek.net "cp ~/jetty/webapps/games.war ~/games.war.bkp"
 
 echo "Copiando o War para servidor remoto"
-scp -i /private/vidageek/games.pem web/target/scala-2.10/games-web* ubuntu@177.71.178.115:~/games.war
+scp -i keys/travis_deploy web/target/scala-2.11/games-web* ubuntu@games.vidageek.net:~/games.war
 
