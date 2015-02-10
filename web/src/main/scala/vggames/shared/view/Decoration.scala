0819337a91c7f5f050862c2ebcd68608145e78ec
Jonas Abreu <jonas@vidageek.net>
2015-02-09 22:02:08 -0200

    updating analytics tracking code

--- web/src/main/scala/vggames/shared/view/Decoration.scala
+++ web/src/main/scala/vggames/shared/view/Decoration.scala
@@ -105,20 +105,14 @@ class Decoration(req: HttpServletRequest, params: Params, session: PlayerSession
   }
 
   def analyticsCode = """
-		var _gaq = _gaq || [];
-		_gaq.push([ '_setAccount', 'UA-1508082-6' ]);
-		_gaq.push([ '_trackPageview' ]);
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		(function() {
-			var ga = document.createElement('script');
-			ga.type = 'text/javascript';
-			ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl'
-					: 'http://www')
-					+ '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0];
-			s.parentNode.insertBefore(ga, s);
-		})();"""
+  ga('create', 'UA-1508082-6', 'auto');
+  ga('send', 'pageview');
+"""
 
   def find(html: String, element: String) =
     html.substring(html.indexOf(s"<$element>") + s"<$element>".length(), html.indexOf(s"</$element>"))
